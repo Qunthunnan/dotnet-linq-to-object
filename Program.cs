c42f5e4bac4479d999b358011256c82ed193c04e
YuriiTrofimenko <tyaa@ukr.net>
2020-11-16 16:08:31 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_linq_to_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
